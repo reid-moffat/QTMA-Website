ca916922829e2e744566c852d8b91f0b96c887d3
Kevin Yu <yu.kevin2002@gmail.com>
2022-01-19 09:52:09 -0500

    removed redirects

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/all_products.html',
-        destination: '/products',
-        permanent: true,
-      },
-    ]
-  }
 }
