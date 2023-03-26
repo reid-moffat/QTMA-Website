0c40d3076c67abb03bc848cb29e116cf20b7df10
Reid Moffat <18rem8@queensu.ca>
2023-03-26 13:01:32 -0400

    team

--- pages/team/index.jsx
+++ pages/team/index.jsx
@@ -2,17 +2,96 @@ import Layout from "../../components/layout";
 import Image from "next/image";
 import FamilyPhoto from "../../public/assets/Members/FamilyPhoto.jpg";
 import Nav from "../Nav";
-import team from "./teamData";
+import { Positions, ClubMembers } from "./teamData.ts";
 import { AiFillLinkedin } from "react-icons/ai";
 import Head from "next/head";
 
-export async function getStaticProps({ params }) {
-    return {
-        props: {},
-    };
-}
+const Team = () => {
+    const MemberCategories = [
+        {
+            name: "Co-Chairs",
+            position: Positions.CoChair,
+        },
+        {
+            name: "Product Managers",
+            position: Positions.ProductManager,
+        },
+        {
+            name: "Senior Developers",
+            position: Positions.SeniorDeveloper,
+        },
+        {
+            name: "Developers",
+            position: Positions.Developer,
+        },
+        {
+            name: "Advisors",
+            position: Positions.Advisor,
+        },
+        {
+            name: "Product Marketing Managers",
+            position: Positions.ProductMarketingManager,
+        },
+        {
+            name: "UI/UX Designers",
+            position: Positions.UIUX,
+        },
+        {
+            name: "Senior Business Analysts",
+            position: Positions.SeniorBA,
+        },
+        {
+            name: "Business Analysts",
+            position: Positions.BA,
+        },
+        {
+            name: "Directors of Operations",
+            position: Positions.Operations,
+        },
+        {
+            name: "Frosh Representatives",
+            position: Positions.FroshRep,
+        },
+        {
+            name: "Web Developers",
+            position: Positions.WebDev,
+        },
+    ];
 
-export default function Team() {
+    const renderClubMembers = () => {
+        const renderSpecialRole = (student) => {
+            if (student.specialRole) {
+                return <p className="special-role">{ student.specialRole }</p>;
+            }
+        };
+
+        return MemberCategories.map(category => (
+            <>
+                <p className="position-name">{category.name}</p>
+                <div className="student-photos">
+                    { ClubMembers
+                        .filter((student) => student.position === category.position)
+                        .map((student, i) => (
+                            <div key={ i } className="student-container">
+                                <Image
+                                    alt="Student photo"
+                                    className="student-image"
+                                    src={ student.image }
+                                    width={ 500 }
+                                    height={ 500 }
+                                />
+                                <p className="student-name">{ student.name }</p>
+                                {renderSpecialRole(student)}
+                                <a className="linkedIn" rel="noreferrer" target="_blank" href={ student.linkedIn }>
+                                    <AiFillLinkedin/>
+                                </a>
+                            </div>
+                        )) }
+                </div>
+            </>
+        ));
+    }
+
     return (
         <>
             <Head>
@@ -29,261 +108,11 @@ export default function Team() {
                             <Image alt="Team Photo" src={ FamilyPhoto } className="family-photo"/>
                         </div>
                     </div>
-                    <p className="position-name">Co-Chairs</p>
-                    <div className="student-photos">
-                        { team
-                            .filter((student) => student.position === "Co-Chair")
-                            .map((student, i) => (
-                                <div key={ i } className="student-container">
-                                    <Image
-                                        alt="Student photo"
-                                        className="student-image"
-                                        src={ student.image }
-                                        width={ 500 }
-                                        height={ 500 }
-                                    />
-                                    <p className="student-name">{ student.name }</p>
-                                    <p className="special-role">{ student.specialRole }</p>
-                                    <a className="linkedIn" rel="noreferrer" target="_blank" href={ student.linkedIn }>
-                                        <AiFillLinkedin/>
-                                    </a>
-                                </div>
-                            )) }
-                    </div>
-                    {/* Product Managers */ }
-                    <p className="position-name">Product Managers</p>
-                    <div className="student-photos">
-                        { team
-                            .filter((student) => student.position === "Product Manager")
-                            .map((student, i) => (
-                                <div key={ i } className="student-container">
-                                    <Image
-                                        alt="Student photo"
-                                        className="student-image"
-                                        src={ student.image }
-                                        width={ 500 }
-                                        height={ 500 }
-                                    />
-                                    <p className="student-name">{ student.name }</p>
-                                    <a className="linkedIn" rel="noreferrer" target="_blank" href={ student.linkedIn }>
-                                        <AiFillLinkedin/>
-                                    </a>
-                                </div>
-                            )) }
-                    </div>
-                    {/* SENIOR DEVELOPERS */ }
-                    <p className="position-name">Senior Developers</p>
-                    <div className="student-photos">
-                        { team
-                            .filter((student) => student.position === "Senior Developer")
-                            .map((student, i) => (
-                                <div key={ i } className="student-container">
-                                    <Image
-                                        alt="Student photo"
-                                        className="student-image"
-                                        src={ student.image }
-                                        width={ 500 }
-                                        height={ 500 }
-                                    />
-                                    <p className="student-name">{ student.name }</p>
-                                    <a className="linkedIn" rel="noreferrer" target="_blank" href={ student.linkedIn }>
-                                        <AiFillLinkedin/>
-                                    </a>
-                                </div>
-                            )) }
-                    </div>
-                    {/* DEVELOPERs */ }
-                    <p className="position-name">Developers</p>
-                    <div className="student-photos">
-                        { team
-                            .filter((student) => student.position === "Developer")
-                            .map((student, i) => (
-                                <div key={ i } className="student-container">
-                                    <Image
-                                        alt="Student photo"
-                                        className="student-image"
-                                        src={ student.image }
-                                        width={ 500 }
-                                        height={ 500 }
-                                    />
-                                    <p className="student-name">{ student.name }</p>
-                                    <a className="linkedIn" rel="noreferrer" target="_blank" href={ student.linkedIn }>
-                                        <AiFillLinkedin/>
-                                    </a>
-                                </div>
-                            )) }
-                    </div>
-                    {/* ADVISORS */ }
-                    <p className="position-name">Advisors</p>
-                    <div className="student-photos">
-                        { team
-                            .filter((student) => student.position === "Advisor")
-                            .map((student, i) => (
-                                <div key={ i } className="student-container">
-                                    <Image
-                                        alt="Student photo"
-                                        className="student-image"
-                                        src={ student.image }
-                                        width={ 500 }
-                                        height={ 500 }
-                                    />
-                                    <p className="student-name">{ student.name }</p>
-                                    <a className="linkedIn" rel="noreferrer" target="_blank" href={ student.linkedIn }>
-                                        <AiFillLinkedin/>
-                                    </a>
-                                </div>
-                            )) }
-                    </div>
-                    {/* PRODUCT MARKETING MANAGERS*/ }
-                    <p className="position-name">Product Marketing Managers</p>
-                    <div className="student-photos">
-                        { team
-                            .filter((student) => student.position === "Product Marketing Manager")
-                            .map((student, i) => (
-                                <div key={ i } className="student-container">
-                                    <Image
-                                        alt="Student photo"
-                                        className="student-image"
-                                        src={ student.image }
-                                        width={ 500 }
-                                        height={ 500 }
-                                    />
-                                    <p className="student-name">{ student.name }</p>
-                                    <a className="linkedIn" rel="noreferrer" target="_blank" href={ student.linkedIn }>
-                                        <AiFillLinkedin/>
-                                    </a>
-                                </div>
-                            )) }
-                    </div>
-                    {/* UI/UX DESIGNERS */ }
-                    <p className="position-name">UI/UX Designers</p>
-                    <div className="student-photos">
-                        { team
-                            .filter((student) => student.position === "UI/UX Designer")
-                            .map((student, i) => (
-                                <div key={ i } className="student-container">
-                                    <Image
-                                        alt="Student photo"
-                                        className="student-image"
-                                        src={ student.image }
-                                        width={ 500 }
-                                        height={ 500 }
-                                    />
-                                    <p className="student-name">{ student.name }</p>
-                                    <a className="linkedIn" rel="noreferrer" target="_blank" href={ student.linkedIn }>
-                                        <AiFillLinkedin/>
-                                    </a>
-                                </div>
-                            )) }
-                    </div>
-                    {/* SENIOR BUSINESS ANALYSTS */ }
-                    <p className="position-name">Senior Business Analysts</p>
-                    <div className="student-photos">
-                        { team
-                            .filter((student) => student.position === "Senior Business Analyst")
-                            .map((student, i) => (
-                                <div key={ i } className="student-container">
-                                    <Image
-                                        alt="Student photo"
-                                        className="student-image"
-                                        src={ student.image }
-                                        width={ 500 }
-                                        height={ 500 }
-                                    />
-                                    <p className="student-name">{ student.name }</p>
-                                    <a className="linkedIn" rel="noreferrer" target="_blank" href={ student.linkedIn }>
-                                        <AiFillLinkedin/>
-                                    </a>
-                                </div>
-                            )) }
-                    </div>
-                    {/* BUSINESS ANALYSTS */ }
-                    <p className="position-name">Business Analysts</p>
-                    <div className="student-photos">
-                        { team
-                            .filter((student) => student.position === "Business Analyst")
-                            .map((student, i) => (
-                                <div key={ i } className="student-container">
-                                    <Image
-                                        alt="Student photo"
-                                        className="student-image"
-                                        src={ student.image }
-                                        width={ 500 }
-                                        height={ 500 }
-                                    />
-                                    <p className="student-name">{ student.name }</p>
-                                    <a className="linkedIn" rel="noreferrer" target="_blank" href={ student.linkedIn }>
-                                        <AiFillLinkedin/>
-                                    </a>
-                                </div>
-                            )) }
-                    </div>
-                    {/* DIRECTORS OF OPERATIONS */ }
-                    <p className="position-name">Directors of Operations</p>
-                    <div className="student-photos">
-                        { team
-                            .filter((student) => student.position === "Director of Operation")
-                            .map((student, i) => (
-                                <div key={ i } className="student-container">
-                                    <Image
-                                        alt="Student photo"
-                                        className="student-image"
-                                        src={ student.image }
-                                        width={ 500 }
-                                        height={ 500 }
-                                    />
-                                    <p className="student-name">{ student.name }</p>
-                                    <p className="special-role">{ student.specialRole }</p>
-                                    <a className="linkedIn" rel="noreferrer" target="_blank" href={ student.linkedIn }>
-                                        <AiFillLinkedin/>
-                                    </a>
-                                </div>
-                            )) }
-                    </div>
-                    {/* FROSH REPRESENTATIVES */ }
-                    <p className="position-name">Frosh Representatives</p>
-                    <div className="student-photos">
-                        { team
-                            .filter((student) => student.position === "Frosh Representatives")
-                            .map((student, i) => (
-                                <div key={ i } className="student-container">
-                                    <Image
-                                        alt="Student photo"
-                                        className="student-image"
-                                        src={ student.image }
-                                        width={ 500 }
-                                        height={ 500 }
-                                    />
-                                    <p className="student-name">{ student.name }</p>
-                                    <a className="linkedIn" rel="noreferrer" target="_blank" href={ student.linkedIn }>
-                                        <AiFillLinkedin/>
-                                    </a>
-                                </div>
-                            )) }
-                    </div>
-                    {/* WEB DEVELOPERS */ }
-                    <p className="position-name">Web Developer</p>
-                    <div className="student-photos">
-                        { team
-                            .filter((student) => student.position === "Web Developer")
-                            .map((student, i) => (
-                                <div key={ i } className="student-container">
-                                    <Image
-                                        alt="Student photo"
-                                        className="student-image"
-                                        src={ student.image }
-                                        width={ 500 }
-                                        height={ 500 }
-                                    />
-                                    <p className="student-name">{ student.name }</p>
-                                    <a className="linkedIn" rel="noreferrer" target="_blank" href={ student.linkedIn }>
-                                        <AiFillLinkedin/>
-                                    </a>
-                                </div>
-                            )) }
-                    </div>
+                    {renderClubMembers()}
                 </div>
             </Layout>
         </>
     );
 }
+
+export default Team;
