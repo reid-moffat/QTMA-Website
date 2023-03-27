7b7e9354b84627f8c11ab784f4a954dad77e8d7a
Reid Moffat <18rem8@queensu.ca>
2023-03-26 20:34:36 -0400

    vercel build error

--- pages/history/index.jsx
+++ pages/history/index.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Founding from "../../public/assets/Data/Founding.png";
 import Expansion from "../../public/assets/Data/Expansion.png";
 import Initiatives from "../../public/assets/Data/Initiatives.png";
-import FamilyPhoto from "../../public/assets/Members/FamilyPhoto.jpg";
+import FamilyPhoto from "../../public/assets/Members/FamilyPhoto.png";
 import Nav from "../Nav";
 import Head from "next/head";
 
