a6fe159742e030055cf48b60ebd26026dddc163a
Reid Moffat <18rem8@queensu.ca>
2023-04-06 19:18:48 -0400

    added all team members

--- data/TeamData.js
+++ data/TeamData.js
@@ -6,24 +6,70 @@ import MatthewMacEachern from "../public/assets/Members/2023-2024/Matthew_MacEac
 import AlisonXia from "../public/assets/Members/2023-2024/Alison_Xia.png";
 import EgorPetrov from "../public/assets/Members/2023-2024/Egor_Petrov.png";
 
+import KevinLiang from "../public/assets/Members/2023-2024/Kevin_Liang.png";
+import OliviaXu from "../public/assets/Members/2023-2024/Olivia_Xu.png";
+import HelenXu from "../public/assets/Members/2023-2024/Helen_Xu.png";
+import MahirHamid from "../public/assets/Members/2023-2024/Mahir_Hamid.png";
+import SriyaNarra from "../public/assets/Members/2023-2024/Sriya_Narra.png";
+import ArchitaSabhani from "../public/assets/Members/2023-2024/Archita_Sabhani.png";
+import MathhewKim from "../public/assets/Members/2023-2024/Mathhew_Kim.png";
+
+import ThomasHuang from "../public/assets/Members/2023-2024/Thomas_Huang.png";
+import GrahamCarkner from "../public/assets/Members/2023-2024/Graham_Carkner.png";
+import AlexLian from "../public/assets/Members/2023-2024/Alex_Lian.png";
+import ManushPatel from "../public/assets/Members/2023-2024/Manush_Patel.png";
+
 import JadeWei from "../public/assets/Members/2023-2024/Jade_Wei.png";
 import ReidMoffat from "../public/assets/Members/2023-2024/Reid_Moffat.png";
 import CainSusko from "../public/assets/Members/2023-2024/Cain_Susko.png";
 import YvesAlikalfic from "../public/assets/Members/2023-2024/Yves_Alikalfic.png";
 
+import JulianBrickman from "../public/assets/Members/2023-2024/Julian_Brickman.png";
+import SophieEllwood from "../public/assets/Members/2023-2024/Sophie_Ellwood.png";
+import MichaelHan from "../public/assets/Members/2023-2024/Michael_Han.png";
+import ConnorRewa from "../public/assets/Members/2023-2024/Connor_Rewa.png";
+import DharsanRavindran from "../public/assets/Members/2023-2024/Dharsan_Ravindran.png";
+import XimingYu from "../public/assets/Members/2023-2024/Ximing_Yu.png";
+import SavannahHan from "../public/assets/Members/2023-2024/Savannah_Han.png";
+import ConnorLeung from "../public/assets/Members/2023-2024/Connor_Leung.png";
+import DanielJoseph from "../public/assets/Members/2023-2024/Daniel_Joseph.png";
+import ShakibAbsar from "../public/assets/Members/2023-2024/Shakib_Absar.png";
+import AlBarrAjiboye from "../public/assets/Members/2023-2024/Al-Barr_Ajiboye.png";
+import MattDobaj from "../public/assets/Members/2023-2024/Matt_Dobaj.png";
+import KeelyMcSpurren from "../public/assets/Members/2023-2024/Keely_McSpurren.png";
+
+import ShabichaSureshkumar from "../public/assets/Members/2023-2024/Shabicha_Sureshkumar.png";
+import AmandaLi from "../public/assets/Members/2023-2024/Amanda_Li.png";
+import ErinZhang from "../public/assets/Members/2023-2024/Erin_Zhang.png";
+import MikeNguyen from "../public/assets/Members/2023-2024/Mike_Nguyen.png";
+import JasmineGao from "../public/assets/Members/2023-2024/Jasmine_Gao.png";
+
+import AidanBrown from "../public/assets/Members/2023-2024/Aidan_Brown.png";
+import BeatriceBeneciaSetiawan from "../public/assets/Members/2023-2024/Beatrice_Benecia_Setiawan.png";
+import StevenVuong from "../public/assets/Members/2023-2024/Steven_Vuong.png";
+import NickArchambault from "../public/assets/Members/2023-2024/Nick_Archambault.png";
+
+import VasiliPiliouras from "../public/assets/Members/2023-2024/Vasili_Piliouras.png";
+import JennieChoi from "../public/assets/Members/2023-2024/Jennie_Choi.png";
+import HannahHe from "../public/assets/Members/2023-2024/Hannah_He.png";
+import WilliamDu from "../public/assets/Members/2023-2024/William_Du.png";
+import AminahRizvan from "../public/assets/Members/2023-2024/Aminah_Rizvan.png";
+import MarcusHui from "../public/assets/Members/2023-2024/Marcus_Hui.png";
+import CynthiaChoi from "../public/assets/Members/2023-2024/Cynthia_Choi.png";
+import CalireHu from "../public/assets/Members/2023-2024/Calire_Hu.png";
+import EmanuelPiccinini from "../public/assets/Members/2023-2024/Emanuel_Piccinini.png";
+
 const Positions = [
     "Co-Chair",
+    "Club Operations",
+    "Product Manager",
     "Senior Developer",
     "Developer",
-    "Advisor",
-    "ProductManager",
-    "ProductMarketingManager",
-    "UIUX",
-    "SeniorBA",
-    "BA",
-    "Operations",
-    "FroshRep",
-    "WebDev",
+    "UI/UX Designer",
+    "Senior Business Analyst",
+    "Business Analyst",
+    "Website Developer",
+    "Frosh Rep",
 ];
 
 const generateMemberData = (image, name, position, linkedin = "", subPosition = "") => {
@@ -46,6 +92,21 @@ const ClubMembers = [
     generateMemberData(AlisonXia, "Alison Xia", "Co-Chair"),
     generateMemberData(EgorPetrov, "Egor Petrov", "Co-Chair"),
 
+    // Club operations
+    generateMemberData(KevinLiang, "Kevin Liang", "Club Operations", "", "Director"),
+    generateMemberData(OliviaXu, "Olivia Xu", "Club Operations", "", "Director"),
+    generateMemberData(HelenXu, "Helen Xu", "Club Operations", "", "Director"),
+    generateMemberData(MahirHamid, "Mahir Hamid", "Club Operations", "", "Senior Advisor"),
+    generateMemberData(SriyaNarra, "Sriya Narra", "Club Operations", "", "Operations"),
+    generateMemberData(ArchitaSabhani, "Archita Sabhani", "Club Operations", "", "Operations"),
+    generateMemberData(MathhewKim, "Mathhew Kim", "Club Operations", "", "Operations"),
+
+    // PMs
+    generateMemberData(ThomasHuang, "Thomas Huang", "Product Manager"),
+    generateMemberData(GrahamCarkner, "Graham Carkner", "Product Manager"),
+    generateMemberData(AlexLian, "Alex Lian", "Product Manager"),
+    generateMemberData(ManushPatel, "Manush Patel", "Product Manager"),
+
     // Sr. devs
     generateMemberData(JadeWei, "Jade Wei", "Senior Developer"),
     generateMemberData(ReidMoffat, "Reid Moffat", "Senior Developer"),
@@ -53,7 +114,6 @@ const ClubMembers = [
     generateMemberData(YvesAlikalfic, "Yves Alikalfic", "Senior Developer"),
 
     // Devs
-    /*
     generateMemberData(JulianBrickman, "Julian Brickman", "Developer"),
     generateMemberData(SophieEllwood, "Sophie Ellwood", "Developer"),
     generateMemberData(MichaelHan, "Michael Han", "Developer"),
@@ -67,252 +127,38 @@ const ClubMembers = [
     generateMemberData(DanielJoseph, "Daniel Joseph", "Developer"),
     generateMemberData(ShakibAbsar, "Shakib Absar", "Developer"),
 
-    generateMemberData(AlBarAjiboye, "Al-Bar Ajiboye", "Developer"),
+    generateMemberData(AlBarrAjiboye, "Al-Barr Ajiboye", "Developer"),
     generateMemberData(MattDobaj, "Matt Dobaj", "Developer"),
     generateMemberData(KeelyMcSpurren, "Keely McSpurren", "Developer"),
-    */
+
+    // UI/UX
+    generateMemberData(ShabichaSureshkumar, "Shabicha Sureshkumar", "UI/UX Designer", "", "Senior Designer"),
+    generateMemberData(AmandaLi, "Amanda Li", "UI/UX Designer", "", "Junior Designer"),
+    generateMemberData(ErinZhang, "Erin Zhang", "UI/UX Designer", "", "Senior Designer"),
+    generateMemberData(MikeNguyen, "Mike Nguyen", "UI/UX Designer", "", "Senior Designer"),
+    generateMemberData(JasmineGao, "Jasmine Gao", "UI/UX Designer", "", "Senior Designer"),
 
     // Sr. BAs
-    /*
-    {
-        image: getImage("Mark_Gragtmans"),
-        name: "Mark Gragtmans",
-        position: Positions.Advisor,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/mark-gragtmans/"
-    },
-    {
-        image: getImage("Arsh_Kochhar"),
-        name: "Arsh Kochhar",
-        position: Positions.Advisor,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/arshkochhar/"
-    },
-    {
-        image: getImage("Jason"),
-        name: "Jason Yang",
-        position: Positions.Advisor,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/jason-z-yang/"
-    },
-    {
-        image: getImage("Zaais_Van_Zyl"),
-        name: "Zaais Van Zyl",
-        position: Positions.Advisor,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/zaais-van-zyl/"
-    },
-    {
-        image: getImage("Richard"),
-        name: "Richard Li",
-        position: Positions.ProductManager,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/richardliqu/"
-    },
-    {
-        image: getImage("Ethan"),
-        name: "Ethan Bonnardeaux",
-        position: Positions.ProductManager,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/ethan-bonnardeaux/"
-    },
-    {
-        image: getImage("Julien_Lin"),
-        name: "Julien Lin",
-        position: Positions.ProductManager,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/julien-lin/"
-    },
-    {
-        image: getImage("Riya_Shah"),
-        name: "Riya Shah",
-        position: Positions.ProductManager,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/riya-shah-queens-university/"
-    },
-    {
-        image: getImage("Vicky_Deng"),
-        name: "Vicky Deng",
-        position: Positions.ProductMarketingManager,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/dengvicky/"
-    },
-    {
-        image: getImage("Chloe_Hung"),
-        name: "Chloe Hung",
-        position: Positions.ProductMarketingManager,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/chloe-hung/"
-    },
-    {
-        image: getImage("Eesha_Kohli"),
-        name: "Eesha Kohli",
-        position: Positions.ProductMarketingManager,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/eesha-kohli/"
-    },
-    {
-        image: getImage("Helen_Xu"),
-        name: "Helen Xu",
-        position: Positions.ProductMarketingManager,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/helencxu/"
-    },
-    {
-        image: getImage("Flora_Lin"),
-        name: "Flora Lin",
-        position: Positions.UIUX,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/~flora/"
-    },
-    {
-        image: getImage("Graham_Carkner"),
-        name: "Graham Carkner",
-        position: Positions.UIUX,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/gcarkner/"
-    },
-    {
-        image: getImage("Randy_Shao"),
-        name: "Randy Shao",
-        position: Positions.UIUX,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/randyshao/"
-    },
-    {
-        image: getImage("Doris_Zhuo"),
-        name: "Doris Zhuo",
-        position: Positions.UIUX,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/doris-zhuo/"
-    },
-    {
-        image: getImage("Dylan_Ratti"),
-        name: "Dylan Ratti",
-        position: Positions.SeniorBA,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/dylan-ratti/"
-    },
-    {
-        image: getImage("Aaron_Chen"),
-        name: "Aaron Chen",
-        position: Positions.BA,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/aaron-chen-44306b135/"
-    },
-    {
-        image: getImage("David_Choo"),
-        name: "David Choo",
-        position: Positions.BA,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/david-j-choo/"
-    },
-    {
-        image: getImage("Mahir_Hamid"),
-        name: "Mahir Hamid",
-        position: Positions.BA,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/mahirhamid/"
-    },
-    {
-        image: getImage("Naomi"),
-        name: "Naomi Junatas",
-        position: Positions.BA,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/naomijunatas/"
-    },
-    {
-        image: getImage("Chris_Power"),
-        name: "Chris Power",
-        position: Positions.BA,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/chris-power-canada/"
-    },
-    {
-        image: getImage("Kevin_Wu"),
-        name: "Kevin Wu",
-        position: Positions.BA,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/wu-kevin/"
-    },
-    {
-        image: getImage("Jacob_Xu"),
-        name: "Jacob Xu",
-        position: Positions.BA,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/jacobx/"
-    },
-    {
-        image: getImage("Dene"),
-        name: "Dene Bezuidenhout",
-        position: Positions.Operations,
-        subPosition: "Marketing",
-        linkedIn: "https://www.linkedin.com/in/denebezuidenhout/"
-    },
-    {
-        image: getImage("Sola_Ebina"),
-        name: "Sola Ebina",
-        position: Positions.Operations,
-        subPosition: "Marketing",
-        linkedIn: "https://www.linkedin.com/in/solaebina/"
-    },
-    {
-        image: getImage("Allen_Chen"),
-        name: "Allen Chen",
-        position: Positions.Operations,
-        subPosition: "Internal Affairs",
-        linkedIn: "https://www.linkedin.com/in/allen-chen0/"
-    },
-    {
-        image: getImage("Benny_Lam"),
-        name: "Benny Lam",
-        position: Positions.Operations,
-        subPosition: "Sponsorships",
-        linkedIn: "https://www.linkedin.com/in/bennylam116/"
-    },
-    {
-        image: getImage("Catherine_Pham"),
-        name: "Catherine Pham",
-        position: Positions.Operations,
-        subPosition: "Sponsorships",
-        linkedIn: "https://www.linkedin.com/in/catherinepham123/"
-    },
-    {
-        image: getImage("Krishaan"),
-        name: "Krishaan Thyagarajan",
-        position: Positions.FroshRep,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/krishaan-thyagarajan/"
-    },
-    {
-        image: getImage("Egor"),
-        name: "Egor Petrov",
-        position: Positions.FroshRep,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/egorpetrov1402/"
-    },
-    {
-        image: getImage("Manush"),
-        name: "Manush Patel",
-        position: Positions.FroshRep,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/manushpatel/"
-    },
-    {
-        image: getImage("Olivia_Xu"),
-        name: "Olivia Xu",
-        position: Positions.FroshRep,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/olivia-xu-057a27171/"
-    },
-    {
-        image: getImage("Kevin_Yu"),
-        name: "Kevin Yu",
-        position: Positions.WebDev,
-        subPosition: "",
-        linkedIn: "https://www.linkedin.com/in/kevin-yu-246147199/"
-    },
-    */
+    generateMemberData(AidanBrown, "Aidan Brown", "Senior Business Analyst"),
+    generateMemberData(BeatriceBeneciaSetiawan, "Beatrice Benecia Setiawan", "Senior Business Analyst"),
+    generateMemberData(StevenVuong, "Steven Vuong", "Senior Business Analyst"),
+    generateMemberData(NickArchambault, "Nick Archambault", "Senior Business Analyst"),
+
+    // BAs
+    generateMemberData(VasiliPiliouras, "Vasili Piliouras", "Business Analyst"),
+    generateMemberData(JennieChoi, "Jennie Choi", "Business Analyst"),
+    generateMemberData(HannahHe, "Hannah He", "Business Analyst"),
+    generateMemberData(WilliamDu, "William Du", "Business Analyst"),
+    generateMemberData(AminahRizvan, "Aminah Rizvan", "Business Analyst"),
+    generateMemberData(MarcusHui, "Marcus Hui", "Business Analyst"),
+    generateMemberData(CynthiaChoi, "Cynthia Choi", "Business Analyst"),
+    generateMemberData(CalireHu, "Calire Hu", "Business Analyst"),
+    generateMemberData(EmanuelPiccinini, "Emanuel Piccinini", "Business Analyst"),
+
+    // Web devs 🙂
+    generateMemberData(ReidMoffat, "Reid Moffat", "Website Developer"),
+    generateMemberData(YvesAlikalfic, "Yves Alikalfic", "Website Developer"),
+    generateMemberData(KevinLiang, "Kevin Liang", "Website Developer"),
 ]
 
 export { Positions, ClubMembers };
