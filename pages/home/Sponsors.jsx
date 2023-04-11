fa78cd3e6af7d257b58334c54d68732c7855e2d9
Reid Moffat <18rem8@queensu.ca>
2023-04-10 20:24:56 -0400

    import sponsors

--- pages/home/Sponsors.jsx
+++ pages/home/Sponsors.jsx
@@ -1,7 +1,11 @@
 import Layout from "../components/layout";
 import Image from "next/image";
 
-const getSponsorImg = (name) => `/assets/Data/Homepage/Sponsors/${name}.png`;
+import ComSoc from "../../public/assets/Data/Homepage/Sponsors/ComSoc.png";
+import McKinsey from "../../public/assets/Data/Homepage/Sponsors/McKinsey.png";
+import QueensArtsci from "../../public/assets/Data/Homepage/Sponsors/Queens_Artsci.png";
+import QueensComputing from "../../public/assets/Data/Homepage/Sponsors/Queens_Computing.png";
+import TrendMicro from "../../public/assets/Data/Homepage/Sponsors/Trend_Micro.png";
 
 export default function Sponsors() {
     return (
@@ -10,20 +14,20 @@ export default function Sponsors() {
                 <h1 className="section-title">Our Sponsors</h1>
                 <div className="sponsors">
                     <div className="sponsor">
-                        <Image alt="Sponsor logo" src={ getSponsorImg('ComSoc') } height={ 155 } width={ 272.02 }/>
+                        <Image alt="Sponsor logo" src={ ComSoc } height={ 155 } width={ 272.02 }/>
                     </div>
                     <div className="sponsor">
-                        <Image alt="Sponsor logo" src={ getSponsorImg('McKinsey') } height={ 155 } width={ 451.59 }/>
+                        <Image alt="Sponsor logo" src={ McKinsey } height={ 155 } width={ 451.59 }/>
                     </div>
                     <div className="sponsor">
-                        <Image alt="Sponsor logo" src={ getSponsorImg('Queens_Artsci') } height={ 155 } width={ 155 }/>
+                        <Image alt="Sponsor logo" src={ QueensArtsci } height={ 155 } width={ 155 }/>
                     </div>
                     <br/>
                     <div className="sponsor">
-                        <Image alt="Sponsor logo" src={ getSponsorImg('Queens_Computing') } height={ 155 } width={ 155 }/>
+                        <Image alt="Sponsor logo" src={ QueensComputing } height={ 155 } width={ 155 }/>
                     </div>
                     <div className="sponsor">
-                        <Image alt="Sponsor logo" src={ getSponsorImg('Trend_Micro') } height={ 155 } width={ 155 }/>
+                        <Image alt="Sponsor logo" src={ TrendMicro } height={ 155 } width={ 155 }/>
                     </div>
                 </div>
             </div>
