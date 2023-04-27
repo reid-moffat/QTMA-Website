84ea57ffacbcf22ec8be5ab30867439e1dc4672e
Reid Moffat <18rem8@queensu.ca>
2023-04-26 20:12:14 -0400

    years

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -50,13 +50,18 @@ export default function Products() {
                         problems.
                     </p>
                     <div className="products-container2">
-                        { productContainer("Eagle", 200, 122) }
-                        { productContainer("Pronto", 120, 116) }
-                        { productContainer("Voluntera", 250, 60) }
+                        { /* 2019 - 2020 */}
                         { productContainer("Stocked", 252.59, 60) }
                         { productContainer("Hungover", 120, 133.13) }
                         { productContainer("Wob", 151.4, 90) }
                         { productContainer("Studii", 176.21, 59) }
+                        { /* 2020 - 2021 */}
+                        { productContainer("Eagle", 200, 122) }
+                        { productContainer("Pronto", 120, 116) }
+                        { productContainer("Voluntera", 250, 60) }
+                        { /* TODO: add loop */}
+                        { /* 2021 - 2022 */}
+                        { /* 2022 - 2023 */}
                     </div>
                 </div>
             </Layout>
