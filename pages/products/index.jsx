3b5f018266ef5238bc5b529515d26779dcc01d32
Reid Moffat <18rem8@queensu.ca>
2023-04-26 20:09:53 -0400

    remove old ones

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -50,7 +50,6 @@ export default function Products() {
                         problems.
                     </p>
                     <div className="products-container2">
-
                         { productContainer("Eagle", 200, 122) }
                         { productContainer("Pronto", 120, 116) }
                         { productContainer("Voluntera", 250, 60) }
@@ -58,76 +57,6 @@ export default function Products() {
                         { productContainer("Hungover", 120, 133.13) }
                         { productContainer("Wob", 151.4, 90) }
                         { productContainer("Studii", 176.21, 59) }
-
-                        <Link passHref href="/product/Eagle">
-                            <div className="product2">
-                                <div className="product-info2">
-                                    <h3>Eagle</h3>
-                                    <p>Your delivery service companion</p>
-                                </div>
-                                <Image alt="Product Logo" src={ getLogo("Eagle", "2020-2021") } width={ 200 } height={ 122 }/>
-                            </div>
-                        </Link>
-
-                        <Link passHref href="/product/Pronto">
-                            <div className="product2">
-                                <div className="product-info2">
-                                    <h3>Pronto</h3>
-                                    <p>Convenient lending for everyone</p>
-                                </div>
-                                <Image alt="Product Logo" src={ getLogo("Pronto", "2020-2021") } width={ 120 } height={ 116 }/>
-                            </div>
-                        </Link>
-
-                        <Link passHref href="/product/Voluntera">
-                            <div className="product2">
-                                <div className="product-info2">
-                                    <h3>Voluntera</h3>
-                                    <p>The one-stop shop for social impact</p>
-                                </div>
-                                <Image alt="Product Logo" src={ getLogo("Voluntera", "2020-2021") } width={ 250 } height={ 60 }/>
-                            </div>
-                        </Link>
-
-                        <Link passHref href="/product/Stocked">
-                            <div className="product2">
-                                <div className="product-info2">
-                                    <h3>Stocked</h3>
-                                    <p>The best a fridge can get.</p>
-                                </div>
-                                <Image alt="Product Logo" src={ getLogo("Stocked", "2020-2021") } width={ 252.59 } height={ 62 }/>
-                            </div>
-                        </Link>
-
-                        <Link passHref href="/product/Hungover">
-                            <div className="product2">
-                                <div className="product-info2">
-                                    <h3>Hungover</h3>
-                                    <p>Taking your pregame to the next level</p>
-                                </div>
-                                <Image alt="Product Logo" src={ getLogo("Hungover", "2019-2020") } width={ 120 } height={ 133.13 }/>
-                            </div>
-                        </Link>
-
-                        <Link passHref href="/product/Wob">
-                            <div className="product2">
-                                <div className="product-info2">
-                                    <h3>Wob</h3>
-                                    <p>Get the latest word on the street.</p>
-                                </div>
-                                <Image alt="Product Logo" src={ getLogo("Wob", "2019-2020") } width={ 151.4 } height={ 90 }/>
-                            </div>
-                        </Link>
-
-                        <Link passHref href="/product/Studii">
-                            <div className="product2">
-                                <div className="product-info2">
-                                    <h3>Studii</h3>
-                                    <p>Academic collaboration at your fingertips</p>
-                                </div>
-                                <Image alt="Product Logo" src={ getLogo("Studii", "2019-2020") } width={ 176.21 } height={ 59 }/>
-                            </div>
-                        </Link>
                     </div>
                 </div>
             </Layout>
