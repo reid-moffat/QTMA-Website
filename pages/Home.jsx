22ce3121f01e9f384673a6ee15fce3074c2eff1b
Yves <yalikalfic@yahoo.com>
2023-05-08 12:06:49 -0400

    first mockup of footer

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,87 +1,109 @@
-import Image from "next/image";
-import qtmaLogo from "../public/assets/Club Data/QTMA_logo.png";
-import startup from "../public/assets/Visuals/Startup_SVG.png";
-import Layout from "../components/layout";
-import { Link as SLink } from "react-scroll";
-import React, { useEffect, useState } from "react";
-
+import Image from 'next/image'
+import qtmaLogo from '../public/assets/Club Data/QTMA_logo.png'
+import startup from '../public/assets/Visuals/Startup_SVG.png'
+import Layout from '../components/layout'
+import { Link as SLink } from 'react-scroll'
+import React, { useEffect, useState } from 'react'
 
 function getWindowDimensions() {
-    if (typeof window !== "undefined") {
-        const { innerWidth: width, innerHeight: height } = window;
-        return {
-            width,
-            height,
-        };
-    }
+  if (typeof window !== 'undefined') {
+    const { innerWidth: width, innerHeight: height } = window
     return {
-        width: 0,
-        height: 0,
-    };
+      width,
+      height,
+    }
+  }
+  return {
+    width: 0,
+    height: 0,
+  }
 }
 
 export function useWindowDimensions() {
-    const [windowdimensions, setWindowDimensions] = useState(getWindowDimensions());
+  const [windowdimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  )
 
-    useEffect(() => {
-        function handleResize() {
-            setWindowDimensions(getWindowDimensions());
-        }
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions())
+    }
 
-        window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize)
 
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
-    return windowdimensions;
+  return windowdimensions
 }
 
 export default function Home() {
-    const { height, width } = useWindowDimensions();
-    const [isMobile, setIsMobile] = useState(false);
-    useEffect(() => {
-        if (width <= 650) {
-            setIsMobile(true);
-        } else {
-            setIsMobile(false);
-        }
-    }, [width]);
+  const { height, width } = useWindowDimensions()
+  const [isMobile, setIsMobile] = useState(false)
+  useEffect(() => {
+    if (width <= 650) {
+      setIsMobile(true)
+    } else {
+      setIsMobile(false)
+    }
+  }, [width])
 
-    return (
-        <Layout background="#0072ff" home={ true }>
-            <section id="home" className="home-container container">
-                <div className="text-container">
-                    <div className="home-logo">
-                        <Image alt="QTMA logo" src={ qtmaLogo.src } width={ 346 } height={ 93.71 }
-                               className="QTMA-logo"/>
-                    </div>
-                    <div className="club-description">
-                        <p>
-                            Queen’s Tech and Media Association (QTMA) { !isMobile && <br/> } Canada’s premier product
-                            development club and tech incubator.
-                        </p>
-                    </div>
-                    { isMobile ? (
-                        <div className="startup-art-container">
-                            <Image alt="illustration" src={ startup } width={ 452 } height={ 462.17 }
-                                   className="startup-art"/>
-                        </div>
-                    ) : (
-                        ""
-                    ) }
-                    <SLink className="CTA" offset={ -104 } to="WhatIsQTMA" smooth={ true } duration={ 100 }>
-                        Learn More
-                    </SLink>
-                </div>
-                { !isMobile ? (
-                    <div className="startup-art-container">
-                        <Image alt="illustration" src={ startup } width={ 452 } height={ 462.17 }
-                               className="startup-art"/>
-                    </div>
-                ) : (
-                    ""
-                ) }
-            </section>
-        </Layout>
-    );
+  return (
+    <Layout background="#0072ff" home={true}>
+      <section id="home" className="home-container container">
+        <div className="text-container">
+          <div className="home-logo">
+            <Image
+              alt="QTMA logo"
+              src={qtmaLogo.src}
+              width={346}
+              height={93.71}
+              className="QTMA-logo"
+            />
+          </div>
+          <div className="club-description">
+            <p>
+              Queen’s Tech and Media Association (QTMA) {!isMobile && <br />}{' '}
+              Canada’s premier product development club and tech incubator.
+            </p>
+          </div>
+          {isMobile ? (
+            <div className="startup-art-container">
+              <Image
+                alt="illustration"
+                src={startup}
+                width={452}
+                height={462.17}
+                className="startup-art"
+              />
+            </div>
+          ) : (
+            ''
+          )}
+          <SLink
+            className="CTA"
+            offset={-104}
+            to="WhatIsQTMA"
+            smooth={true}
+            duration={100}
+          >
+            Learn More
+          </SLink>
+        </div>
+        {!isMobile ? (
+          <div className="startup-art-container">
+            <Image
+              alt="illustration"
+              src={startup}
+              width={452}
+              height={462.17}
+              className="startup-art"
+            />
+          </div>
+        ) : (
+          ''
+        )}
+      </section>
+    </Layout>
+  )
 }
