22114bef6521ab73139eee65eed81c19f19d486e
Reid Moffat <18rem8@queensu.ca>
2023-09-10 17:27:50 -0400

    individual products too

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import productData from "../../data/ProductsData";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import Footer from "../../components/Footer";
 
 //
 // This file is used to dynamically render each product (e.g. <Link passHref href="/product/Eagle>">)
@@ -128,6 +129,7 @@ export default function Product() {
 					</div>
 				)}
 			</Layout>
+			<Footer/>
 		</>
 	);
 }
