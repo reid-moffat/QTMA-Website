20b936d6938824ee8b97e0fbb3d109f3a78f0c8a
Reid Moffat <18rem8@queensu.ca>
2024-01-30 12:55:14 -0500

    move reports to contact

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -5,6 +5,7 @@ import Nav from "../Nav";
 import React from "react";
 import Head from "next/head";
 import Footer from "../../components/Footer";
+import { HiOutlineDocumentDownload } from "react-icons/hi";
 
 export default function Contact() {
 	return (
@@ -20,19 +21,36 @@ export default function Contact() {
 			<Nav/>
 			<Layout background="#EDF5FC">
 				<div className="contact-container container">
-					<form className="contact-info">
+					<div className="contact-info">
 						<div className="contact-title-container">
 							<h1 className="section-title contact-title">Contact Us</h1>
 							<p className="contact-subheading">
 								You can reach us by sending a message to QTMA's instagram, {""}
-								<a href="https://www.instagram.com/queenstechmedia" style={{ color: "#0000EE" }}>@queenstechmedia</a>
+								<a href="https://www.instagram.com/queenstechmedia"
+								   style={{ color: "#0000EE" }}>@queenstechmedia</a>
 							</p>
+							<a href="https://www.instagram.com/queenstechmedia" target="_blank">
+								<Image alt="instagram" src={Instagram} width={180} height={180}/>
+							</a>
 						</div>
-					</form>
-					<div className="instagram-illustration">
-						<a href="https://www.instagram.com/queenstechmedia" target="_blank">
-							<Image alt="instagram" src={Instagram} width={400} height={400}/>
-						</a>
+					</div>
+					<div className="contact-info">
+						<div className="contact-title-container">
+							<h1 className="section-title contact-title">QTMA guide</h1>
+							<p className="contact-subheading">
+								See our recruiting guide for a comprehensive overview of the club
+							</p>
+							<a
+								className="recruiting-container"
+								href="/assets/Club Data/Info/2023_Hiring_Package.pdf"
+								target="_blank"
+							>
+								<p>Recruiting Guide</p>
+								<p className="download-icon">
+									<HiOutlineDocumentDownload></HiOutlineDocumentDownload>
+								</p>
+							</a>
+						</div>
 					</div>
 				</div>
 			</Layout>
